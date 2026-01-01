569efd944bcaa76b617d19e978b9577edc4ef3ae
Ubuntu <ubuntu@bclu2-p1-8jhrm-tvxh4.local>
2026-01-01 18:07:51 +0000

    fix

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-~/fc-lab/00-host-prereqs.sh
-~/fc-lab/10-install-firecracker.sh
+./00-host-prereqs.sh
+./10-install-firecracker.sh
 export PATH="$HOME/.local/bin:$PATH"
-~/fc-lab/20-build-nginx-rootfs.sh
-FOLLOW_BOOT_SECONDS=15 ~/fc-lab/30-run-nginx-microvm.sh
+./20-build-nginx-rootfs.sh
+FOLLOW_BOOT_SECONDS=15 ./30-run-nginx-microvm.sh
